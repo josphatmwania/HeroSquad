149bf57e1d7c5e95b1f8b4f2ade40115ae65e25b
josphatmwania <josphat.muyo@student.moringaschool.com>
2021-08-13 01:21:54 +0300

    Add Squad register method.

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -8,7 +8,7 @@ public class Squad {
     private String squadName;
     private int squadID;
     private static List<Squad> squadList = new ArrayList<>();
-    private list<Hero> heroMembers new ArrayList<>();
+    private list<Hero> heroMembers = new ArrayList<>();
     private String cause;
     private boolean isSquadFull = false;
     private static boolean isRegisteredHero = false;
@@ -32,9 +32,34 @@ public class Squad {
 
         }
 
+    public String getName() {
+            return squadName;
+        }
+
+    public int getSquadId() {
+            return squadId;
+        }
+
+// Squad size
+    public void addMembers(Hero hero) {
+            if (heroMembers.size() >=3 ) {
+                isSquadFull = true;
+
+            } else {
+                heroMembers.add(hero);
+            }
+        }
+//  Full Squad
+
+    public boolean getSquadFull() {
+            return isSquadFull;
+
+        }
+
+
+
+
 
     }
 
 
-
-}
--- src/test/java/models/SquadTest.java
+++ src/test/java/models/SquadTest.java
@@ -1,4 +1,51 @@
 package models;
 
+import static org.junit.Assert.;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.Before;
+
+
+@SuppressWarnings("ALL")
+
 public class SquadTest {
+    @Before
+    public void setUp() throws Exception{
+
+    }
+
+
+// Testting a New Hero
+
+    private Hero setNewHero(){
+        return new Hero("Bazu", 15, "Thriller", "Scary" );
+    }
+
+    private Hero setUpHero2(){
+        return new Hero("Batman", 35, "Monster", "Beast");
+
+    }
+
+
+// Tests for Squad
+
+//    @Test
+//    public void squadInstaciatedPerfect() {
+//        Squad squad = setUpNewSquad(setUpNewHero());
+//        assertTrue(squad instanceof Squad);
+//
+//    }
+    @Test
+    public void getSquadName(){
+        Squad squad = setupNewSquad(setNewHero());
+        assertTrue(squad.getName() instanceof String);
+    }
+
+
+
+
+
+
+
 }
